c1c6a6de5e0ceb6d2cccf3786ff587703e9ccd46
Anton Krug <anton.krug@microsemi.com>
2018-08-20 16:53:54 +0100

    Trying both Debug and Release

--- tests/debugWithOpenOCD.sh
+++ tests/debugWithOpenOCD.sh
@@ -12,7 +12,9 @@ sleep 4 # give enough time for hardware to init
 echo "OpenOCD should be launched, start the gdb now:"
 
 echo "Go into first Debug folder I can find (make sure to run clean before this so there are no other folders present)"
+# One of these below should success
 cd $TESTS_DIR/../Debug*
+cd $TESTS_DIR/../Release*
 $SC_DIR/riscv-unknown-elf-gcc/bin/riscv64-unknown-elf-gdb -x ../tests/gdb-test-checksum *.elf
 
 # Store the exit code
